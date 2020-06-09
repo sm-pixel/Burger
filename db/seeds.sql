117ca173e9de7d2cd41c40cf5108e45df402a2af
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-09 14:59:24 -0400

    added schema and seeds sql files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("California Burger");
