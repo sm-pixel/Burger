85046509b2b89ca35c6ae8f9996a336afd00686e
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-11 18:09:07 -0400

    trying to get buttons working

---
+++ public/assets/js/burger.js
@@ -0,0 +1,30 @@
+$(function () {
+    //new burger form and button
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var burger_name = $("#name")
+        $.ajax("/burger", {
+            type: "POST",
+            data: burger_name
+        }).then(function () {
+            console.log("new burger");
+            location.reload();
+        })
+    })
+    
+    //devour it button
+    $(".devoured").on("click", function(event) {
+        event.preventDefault();
+        
+        var id = $(this).data("id");
+        $ajax("/burger" + id, {
+            type: "PUT"
+        }).then(
+            function() {
+                console.log("devoured");
+                location.reload();
+            }
+        )
+    })
+});
