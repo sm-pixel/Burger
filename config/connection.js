3121e831644eb6697db49dabbfb3f2488d3689ff
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-11 22:31:46 -0400

    updated connection to work with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //Dependencies
 const mysql = require("mysql");
 //Create Connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burgers_db"
-});
+let connection
+if (process.env.JAWSDB_URL) {
+  connection - mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
