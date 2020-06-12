1c93c4174a1ce277632a702302d58254585cefe8
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-11 22:00:03 -0400

    changed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
 INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
-INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
 INSERT INTO burgers (burger_name) VALUES ("California Burger");
